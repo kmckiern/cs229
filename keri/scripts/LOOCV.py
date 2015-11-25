44a0b026169eb3130ab33ca54a954f7324cc272d
kmckiern <kmckiern@stanford.edu>
2015-11-25 12:19:42 -0800

    remove debug print statements

--- keri/scripts/LOOCV.py
+++ keri/scripts/LOOCV.py
@@ -26,7 +26,6 @@ def main():
     dwn0 = np.array(cands['dwn0'])
     dwn1 = np.array(cands['dwn1'])
 
-    print ('meh', data.T.shape, dwn0.shape)
     # train via LOOCV
     nc = cands.shape[1]
     loo = cross_validation.LeaveOneOut(nc)
