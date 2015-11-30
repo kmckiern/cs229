0f4c85785b454aee3d0ad8bfad3fab5a69f7e44b
kmckiern <kmckiern@stanford.edu>
2015-11-29 16:53:17 -0800

    plot dwn score distribution

--- keri/scripts/LOOCV.py
+++ keri/scripts/LOOCV.py
@@ -15,16 +15,31 @@ parser.add_argument('--fm', type=str, help='feature matrix',
         default='../../data/features/cand_parse_all_2014_feat_matrix.pkl')
 parser.add_argument('--cf', type=str, help='file of candidates and'
         'CIDs', default='../../joe/out/test_update.dat')
+parser.add_argument('--pltd', action='store_true', help='plot dwn' 
+        'distribution', default=False)
+parser.add_argument('--ipnb', action='store_true', help='open ipython'
+        'notebook', default=False)
 args, unknown = parser.parse_known_args()
 
 def main():
     # read in data
     data = pd.read_pickle(args.fm)
-    cols = ['name', 'state', 'CID', 'party', 'dwn0', 'dwn1']
+    cols = ['name', 'state', 'CID', 'party', 'DWN-0', 'DWN-1']
     cands = pd.read_csv(args.cf, sep='\t', names=cols)
     cids = np.array(cands['CID'])
-    dwn0 = np.array(cands['dwn0'])
-    dwn1 = np.array(cands['dwn1'])
+    dwn0 = np.array(cands['DWN-0'])
+    dwn1 = np.array(cands['DWN-1'])
+    party = np.array(cands['party'])
+
+    if args.pltd:
+        import seaborn as sns 
+        cm = sns.diverging_palette(20,220,n=2) #'coolwarm'
+        sns.set(font_scale=.8)
+        sns.set_style(style='white')
+        # IPython.embed()
+        party_df = sns.lmplot(x='DWN-0', y='DWN-1', hue='party', data=cands,
+                fit_reg=False, palette=cm)
+        party_df.savefig('../../data/out/dwn.png', dpi=400)
 
     # train via LOOCV
     nc = cands.shape[0]
@@ -43,7 +58,8 @@ def main():
     svr = np.array(svr)
     svr_min_err = svr[svr[:,1].argmin()]
 
-    IPython.embed()
+    if args.ipnb:
+        IPython.embed()
 
 if __name__ == '__main__':
     main()
