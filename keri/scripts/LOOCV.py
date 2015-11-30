b71aa6c040aa768508217e04f4b2bec22c1120e3
Joe Napoli <JNapoli@stanford.edu>
2015-11-29 18:50:57 -0800

    Cross validation and hyperparameter stuff

--- keri/scripts/LOOCV.py
+++ keri/scripts/LOOCV.py
@@ -5,10 +5,13 @@ import argparse
 import numpy as np
 import pandas as pd
 import os
-import sys
+import scipy.stats as stats
+import sys, time
 sys.path.append('../../joe/scripts/')
 from train import *
-from sklearn import cross_validation
+from sklearn import cross_validation, svm
+from sklearn.grid_search import RandomizedSearchCV, GridSearchCV
+from operator import itemgetter
 
 parser = argparse.ArgumentParser(description='LOOCV over dataset')
 parser.add_argument('--fm', type=str, help='feature matrix',
@@ -51,6 +54,59 @@ def main():
         Y_train, Y_test = dwn0[train_index], dwn0[test_index]
         lr.append(linear_regression(X_train, Y_train, X_test, Y_test))
         svr.append(SVR(X_train, Y_train, X_test, Y_test))
+
+    # cross-validation and hyperparameter optimization
+    X_raw, Y_raw, Y2_raw = np.array(data), dwn0.T, dwn1.T
+    
+    # partition into training and test set
+    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X_raw, Y_raw, test_size=0.2, random_state=0)
+    dat_iterable = cross_validation.train_test_split(X_raw, Y_raw, test_size=0.2, random_state=0)
+
+    def report(grid_scores, n_top=10):
+        top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+        for i, score in enumerate(top_scores):
+            print("Model with rank: {0}".format(i + 1))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format( \
+                score.mean_validation_score, np.std(score.cv_validation_scores)))
+            print("Parameters: {0}".format(score.parameters)) 
+            print("")
+        return score.parameters
+
+    # randomized parameter search dat shit
+    classifier = svm.SVR()
+
+    # parameter distributions
+    param_dist = {'degree': [2,3,4,5],
+                  'kernel': ['linear','poly','rbf','sigmoid'],
+                  'C': np.arange(90,130)}
+
+    # create the search
+    n_iter_search = 100
+    random_search = GridSearchCV(classifier, 
+                                 param_dist, 
+                                 #n_iter=n_iter_search,
+                                 # cv=number of folds
+                                 cv=5,
+                                 n_jobs=8)
+    t0 = time.time()
+    # for SVC, need to convert 
+    # dw array to a boolean array for classification
+    y_temp = []
+    for i in Y_raw:
+        if i > 0: y_temp.append(1)
+        else: y_temp.append(0)
+    y_temp = np.array(y_temp)
+
+    # perform the search
+    random_search.fit(X_raw, Y_raw)
+    t1 = time.time()
+    print("RandomizedSearchCV took %.2f seconds for %d candidates" \
+                  " parameter settings." % ((t1 - t0), n_iter_search))
+    params_2_use = report(random_search.grid_scores_)
+    clf_new = svm.SVR(kernel=params_2_use['kernel'], 
+                      C=params_2_use['C'],
+                      degree=params_2_use['degree']).fit(X_train, Y_train)
+    print(clf_new.score(X_test, Y_test))
 
     # get minimum error results
     lr = np.array(lr)
