bfd5098b5473b26ffa4ca57781a96f8d6a37fbc7
Joe Napoli <JNapoli@stanford.edu>
2015-12-06 00:02:31 -0800

    grid search updated

--- keri/scripts/grid_search.py
+++ keri/scripts/grid_search.py
@@ -15,7 +15,7 @@ from operator import itemgetter
 
 parser = argparse.ArgumentParser(description='train the modelz')
 parser.add_argument('--fm', type=str, help='feature matrix',
-        default='../../data/features/cand_parse_all_2014_feat_matrix_trim_normed.pkl')
+        default='../../data/features/cand_parse_all_fresh_2010_feat_matrix_trim_normed.pkl')
 parser.add_argument('--cf', type=str, help='file of candidates and CIDs', default='../../joe/out/cand_parse_all.dat')
 parser.add_argument('--pltd', action='store_true', help='plot dwn distribution', default=False)
 parser.add_argument('--ipnb', action='store_true', help='open ipython notebook', default=False)
@@ -34,27 +34,23 @@ def report(grid_scores, n_top=10):
 
 def main():
     # read in data
-    data = pd.read_pickle(args.fm)
-    data_2 = pd.read_pickle('../../data/features/cand_parse_all_2012_feat_matrix_trim_normed.pkl')
-    data_3 = pd.read_pickle('../../data/features/cand_parse_all_2010_feat_matrix_trim_normed.pkl') 
-    data = pd.concat((data, data_2, data_3))
+    data_2010 = np.array( pd.read_pickle(args.fm) )
+    data_2012 = np.array( pd.read_pickle('../../data/features/cand_parse_all_fresh_2012_feat_matrix_trim_normed.pkl') )
+    data_2014 = np.array( pd.read_pickle('../../data/features/cand_parse_all_fresh_2014_feat_matrix_trim_normed.pkl') )
 
-    data_IDs = list( data.index )
+    scores_2010 = np.array( pd.read_pickle('../../data/features/cand_parse_all_fresh_2010_scores.pkl') )
+    scores_2012 = np.array( pd.read_pickle('../../data/features/cand_parse_all_fresh_2012_scores.pkl') )
+    scores_2014 = np.array( pd.read_pickle('../../data/features/cand_parse_all_fresh_2014_scores.pkl') )
 
-    cols = ['f name', 'l name', 'state', 'party', 'CID', 'DWN-0', 'DWN-1']
-    cands = pd.read_csv(args.cf, sep='\t', names=cols)
-
-    dw0 = []
-    dw1 = []
-
-    for id1 in data_IDs:
-        for i, id2 in enumerate( cands['CID'] ):
-            if id1 == id2:
-                dw0.append(cands['DWN-0'][i])
-                dw1.append(cands['DWN-1'][i])
-
-    dwn0 = np.array(dw0)
-    dwn1 = np.array(dw1)
+    set1 = (data_2010, scores_2010[:,0], scores_2010[:,1])
+    set2 = (np.concatenate((data_2010, data_2012), axis=0),
+            np.concatenate((scores_2010[:,0], scores_2012[:,0]), axis=0),
+            np.concatenate((scores_2010[:,1], scores_2012[:,1]), axis=0)
+           )
+    set3 = (np.concatenate((data_2010, data_2012, data_2014), axis=0),
+            np.concatenate((scores_2010[:,0], scores_2012[:,0], scores_2014[:,0]), axis=0),
+            np.concatenate((scores_2010[:,1], scores_2012[:,1], scores_2014[:,1]), axis=0)
+           )
 
     if args.pltd:
         import seaborn as sns 
@@ -68,90 +64,93 @@ def main():
         plt.title('distribution of DW-NOMINATE scores by party')
         party_df.savefig('../../data/out/dwn_nooutlier.png', dpi=400, bbox_inches='tight')
 
-    # training set and tragets 
-    X_raw, DWN_0, DWN_1 = np.array(data), dwn0.T, dwn1.T
-    
-    # partition data into training and test set for each dwn score
-    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X_raw, DWN_0, test_size=0.4, random_state=0)
-    X_train_2, X_test_2, Y_train_2, Y_test_2 = cross_validation.train_test_split(X_raw, DWN_1, test_size=0.4, random_state=0)
+    for set in [set1, set2, set3]:
 
-    print('Training set size: %d samples' % len(X_train))
-    print('Test set size: %d samples' % len(X_test))
-    print('')
+        # training set and tragets 
+        X_raw, DWN_0, DWN_1 = set[0], set[1].T, set[2].T
+        
+        # partition data into training and test set for each dwn score
+        X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X_raw, DWN_0, test_size=0.4, random_state=0)
+        X_train_2, X_test_2, Y_train_2, Y_test_2 = cross_validation.train_test_split(X_raw, DWN_1, test_size=0.4, random_state=0)
 
-    # define grid spacing here
-    Cs = np.arange(1,200,1)
-    gammas = [1e1, 5, 1e0, 0.5, 1e-1, 0.05, 1e-2, 0.005, 1e-3, 0.0005, 1e-4, 0.00005, 1e-5, 0.000005, 1e-6]
+        print('Training set size: %d samples' % len(X_train))
+        print('Test set size: %d samples' % len(X_test))
+        print('')
 
-    # parameter distributions. initially we will just search over
-    # different orders of magnitude of the parameters.
-    param_grid = [{'kernel': ['rbf'], 
-                    'gamma': gammas,
-                        'C': Cs
-                   },
+        # define grid spacing here
+        Cs = np.arange(1,200,10)
+        #gammas = [1e1, 5, 1e0, 0.5, 1e-1, 0.05, 1e-2, 0.005, 1e-3, 0.0005, 1e-4, 0.00005, 1e-5, 0.000005, 1e-6]
+        gammas = [1e1, 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
 
-                  {'kernel': ['linear'],
-                        'C': Cs
-                   },
+        # parameter distributions. initially we will just search over
+        # different orders of magnitude of the parameters.
+        param_grid = [{'kernel': ['rbf'], 
+                        'gamma': gammas,
+                            'C': Cs
+                       },
 
-                  {'kernel': ['poly'],
-                        'C': Cs,
-                    'gamma': gammas,  
-                   'degree': [2, 3, 4, 5]
-                   }
-                 ]
+                      {'kernel': ['linear'],
+                            'C': Cs
+                       },
 
-    # create the search, one for each DWN dimension.
-    grid_search_0 = GridSearchCV(svm.SVR(), param_grid, cv=5, n_jobs=4)
-    grid_search_1 = GridSearchCV(svm.SVR(), param_grid, cv=5, n_jobs=4)
-    grid_search_SVC = GridSearchCV(svm.SVC(), param_grid, cv=5, n_jobs=4)
-    t0 = time.time()
+                      {'kernel': ['poly'],
+                            'C': Cs,
+                        'gamma': gammas,  
+                       'degree': [2, 3, 4]
+                       }
+                     ]
 
-    # for SVC, need to convert 
-    # dw array to a boolean array for classification
-    Y_train_SVC, Y_test_SVC = [], []
-    
-    for i in range(len(Y_train)):
-        if Y_train[i] > 0: Y_train_SVC.append(1)
-        else: Y_train_SVC.append(0)
+        # create the search, one for each DWN dimension.
+        grid_search_0 = GridSearchCV(svm.SVR(), param_grid, cv=5, n_jobs=4)
+        grid_search_1 = GridSearchCV(svm.SVR(), param_grid, cv=5, n_jobs=4)
+        grid_search_SVC = GridSearchCV(svm.SVC(), param_grid, cv=5, n_jobs=4)
+        t0 = time.time()
 
-    for i in range(len(Y_test)):   
-        if Y_test[i] > 0: Y_test_SVC.append(1)
-        else: Y_test_SVC.append(0)
-    
-    Y_train_SCV, Y_test_SVC = np.array(Y_train_SVC), np.array(Y_test_SVC)
+        # for SVC, need to convert 
+        # dw array to a boolean array for classification
+        Y_train_SVC, Y_test_SVC = [], []
+        
+        for i in range(len(Y_train)):
+            if Y_train[i] > 0: Y_train_SVC.append(1)
+            else: Y_train_SVC.append(0)
 
-    print('Performing search...')
-    # perform the search on development subset of data
-    grid_search_0.fit(X_train, Y_train)
-    grid_search_1.fit(X_train_2, Y_train_2)
-    grid_search_SVC.fit(X_train, Y_train_SVC)
-    print('GridSearchCV took % .2f seconds.' % (time.time() - t0))
-    print('')
+        for i in range(len(Y_test)):   
+            if Y_test[i] > 0: Y_test_SVC.append(1)
+            else: Y_test_SVC.append(0)
+        
+        Y_train_SCV, Y_test_SVC = np.array(Y_train_SVC), np.array(Y_test_SVC)
 
-    print('Grid search over DWN0:')
-    report(grid_search_0.grid_scores_)
-    print('')
+        print('Performing search...')
+        # perform the search on development subset of data
+        grid_search_0.fit(X_train, Y_train)
+        grid_search_1.fit(X_train_2, Y_train_2)
+        grid_search_SVC.fit(X_train, Y_train_SVC)
+        print('GridSearchCV took % .2f seconds.' % (time.time() - t0))
+        print('')
 
-    print('Grid search for DWN1:')
-    report(grid_search_1.grid_scores_)
-    print('')
+        print('Grid search over DWN0:')
+        report(grid_search_0.grid_scores_)
+        print('')
 
-    print('Grid search for SVC:')
-    report(grid_search_SVC.grid_scores_)
-    print('')
+        print('Grid search for DWN1:')
+        report(grid_search_1.grid_scores_)
+        print('')
 
-    # use best performing training model to estimate the test set error
-    clf_best_0 = grid_search_0.best_estimator_
-    clf_best_1 = grid_search_1.best_estimator_
-    clf_best_SVC = grid_search_SVC.best_estimator_
+        print('Grid search for SVC:')
+        report(grid_search_SVC.grid_scores_)
+        print('')
 
-    # k-fold cross validation estimates the test score
-    test_score_0 = np.average( cross_validation.cross_val_score(clf_best_0, X_test, y=Y_test, cv=2) )
-    test_score_1 = np.average( cross_validation.cross_val_score(clf_best_1, X_test_2, y=Y_test_2, cv=2) )
-    test_score_SVC = np.average( cross_validation.cross_val_score(clf_best_SVC, X_test, y=Y_test_SVC, cv=2) )
-    print('Test scores: (%.4f, %.4f, %.4f) for DWN0, DWN1, and SVC, respectively.' % (test_score_0, test_score_1, test_score_SVC))
-    print('Done.')
+        # use best performing training model to estimate the test set error
+        clf_best_0 = grid_search_0.best_estimator_
+        clf_best_1 = grid_search_1.best_estimator_
+        clf_best_SVC = grid_search_SVC.best_estimator_
+
+        # k-fold cross validation estimates the test score
+        test_score_0 = clf_best_0.score(X_test, Y_test) #np.average( cross_validation.cross_val_score(clf_best_0, X_test, y=Y_test, cv=2) )
+        test_score_1 = clf_best_1.score(X_test, Y_test_2) #np.average( cross_validation.cross_val_score(clf_best_1, X_test_2, y=Y_test_2, cv=2) )
+        test_score_SVC = clf_best_SVC.score(X_test, Y_test_SVC) #np.average( cross_validation.cross_val_score(clf_best_SVC, X_test, y=Y_test_SVC, cv=2) )
+        print('Test scores: (%.4f, %.4f, %.4f) for DWN0, DWN1, and SVC, respectively.' % (test_score_0, test_score_1, test_score_SVC))
+        print('Done.')
 
     if args.ipnb:
         IPython.embed()
